a4e4be0e4df4dd531c310e4db8f944569e979381
mpostument <777rip777@gmail.com>
2020-10-21 18:02:04 +0300

    added different way to organize code

---
+++ ThreeWays/Pages/CounterBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ThreeWays.Pages
+{
+    public class CounterBase: ComponentBase
+    {
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
---
+++ ThreeWays/Pages/FetchData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ThreeWays.Data;
+    
+namespace ThreeWays.Pages
+{
+    public partial class  FetchData
+    {
+        private WeatherForecast[] forecasts;
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
